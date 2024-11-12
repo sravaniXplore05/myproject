696d0808f36517869aa4eea303056e793ca7e7ab
kothapalli sai sravani <sravanik0105@gmail.com>
2024-11-12 22:02:59 +0530

    commit

---
+++ first programme.py
@@ -0,0 +1,35 @@
+"""num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+if num1>num2 and num1>num3:
+         print(f" the greatest number is {num1}")
+elif num2>num3:
+    print(f"the greatest number is {num2}")
+else:
+    print(f"the greatest number is {num3}")
+"""
+
+          
+"""num=int(input("enter number"))
+c=0
+for i in range(1,5):
+    if num%i==0:
+        c=c+1
+if c==2:
+    print(f"given {num} is prime")
+else:
+    print(f" given {num} is not prime")
+"""
+
+"""complex1=complex(input("enter first complex number(in the form a+bj)"))
+complex2=complex(input("enter second complex number(in the form a+bj)"))
+sum_result=complex1+complex2
+multiply_result=complex1*complex2
+print(f"addition of {complex1} and {complex2} is:{sum_result}")
+print(f"multplication of {complex1} and {complex2} is:{multiply_result}")
+"""
+
+"""num=int(input("enter number"))
+for i in range(1,11):
+    print(f" {num}*{i}={num*i}")
+"""    
